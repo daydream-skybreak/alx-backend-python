b6ca63622d2b6f0e0a1edf1e1d48d35e268b215c
Samuel <samueldesta323@gmail.com>
2023-02-02 18:25:57 +0300

    added file 4

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-'''creating typed variables'''
+'''Task 4's module.
+'''
+
 
 a: int = 1
 pi: float = 3.14
-i_understand_annotation: bool = True
+i_understand_annotations: bool = True
 school: str = 'Holberton'
